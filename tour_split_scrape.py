7c0420de88c23d5c98b679d8f2ea43efb009a680
Vincent Acocella <152810231+vacocella@users.noreply.github.com>
2024-10-03 16:52:39 -0400

    Update tour_split_scrape.py

--- tour_split_scrape.py
+++ tour_split_scrape.py
@@ -13,6 +13,7 @@ import pdb
 from urllib.parse import urlparse, parse_qs
 from datetime import datetime
 from datetime import date
+import re
 
 # ----------------------- Configuration -----------------------
 
@@ -28,8 +29,9 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 # MongoDB connection string
 # MONGODB_URI = os.getenv('MONGODB_URI')
 
-tour_url = 'https://www.vlr.gg/gc-2024'
-# tour_url = 'https://www.vlr.gg/vct-2024'
+all_tours = ['https://www.vlr.gg/vct-2024','https://www.vlr.gg/gc-2024',
+'https://www.vlr.gg/vcl-2024',
+]
 
 # ----------------------- Relational Database Setup (PostgreSQL) -----------------------
 
@@ -53,7 +55,7 @@ class Team(Base):
     team_id = Column(Integer, primary_key=True)
     team_name = Column(String(100))
     region_id = Column(Integer, ForeignKey('regions.region_id'))
-    founding_date = Column(Date, nullable=True)
+    team_img_url =  Column(String(100))
     active = Column(Boolean, default=True)
 
     region = relationship('Region')
@@ -269,6 +271,18 @@ agent_names = [
 # games_collection = mongo_db['games']
 
 # ----------------------- Helper Functions -----------------------
+
+
+def valid_img_url(img_url):
+    if "owcdn" in img_url:
+        # Use regex to extract the ID before the file extension
+        match = re.search(r'([^/]+)(?=\.\w+$)', img_url)
+        if match:
+            return match.group(0)
+        else:
+                return ""
+    else:
+        return ""    
 
 def extract_player_id_from_url(player_url):
 
@@ -519,6 +533,8 @@ def get_team(team_link):
     team_soup = BeautifulSoup(team_res.content, 'html.parser')
 
     team_header = team_soup.find('div', class_='team-header')
+    avatar_div = team_soup.find('div', class_='wf-avatar')
+    team_img = avatar_div.find('img')['src']
     team_name = team_header.find('h1', class_='wf-title').text.strip()
 
     # Get region (you may need to adjust this based on actual data available)
@@ -529,7 +545,7 @@ def get_team(team_link):
     existing_team = session.query(Team).filter_by(team_id=team_id).first()
     if not existing_team:
         # Insert team into database
-        new_team = Team(team_id=team_id, team_name=team_name, region_id=region_id)
+        new_team = Team(team_id=team_id, team_name=team_name, region_id=region_id, team_img_url= valid_img_url(team_img))
         session.add(new_team)
         session.commit()
         print(f"Inserted team {team_name} (ID: {team_id}) into PostgreSQL.")
@@ -828,7 +844,7 @@ def scrape_player_page(player_url):
         player_id=player_id,
         name=player_name,
         real_name=player_name_real,
-        pp_url= player_img_url,
+        pp_url= valid_img_url(player_img_url),
         # team_id=team_id,
         region_id=region_id
     )
@@ -911,7 +927,7 @@ def scrape_split(split_url, tour_id):
     except Exception as e:
         print(e)
 
-def scrape_tour_data():
+def scrape_tour_data(tour_url):
     response = requests.get(tour_url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -921,8 +937,9 @@ def scrape_tour_data():
     tour_link = tour_url 
     tour_id = get_tour(tour_title.strip(), tour_link)
         
-    events = soup.find_all('a', class_='wf-card mod-flex event-item')
-    print(tour_id)
+    event_divs = soup.find_all('div', class_='events-container-col')
+    events = event_divs[1].find_all('a', class_='wf-card mod-flex event-item')
+
     for row in events:
         try:
             print("HREF",row['href'])
@@ -935,7 +952,8 @@ def scrape_tour_data():
 
 if __name__ == "__main__":
     try:
-        scrape_tour_data()
+        for tour_url in all_tours:
+            scrape_tour_data(tour_url)
     except Exception as e:
         print(f"An error occurred: {e}")
     finally:
